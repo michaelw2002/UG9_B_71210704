4a2632ec20536cec11dcf78cca711aeeea31739e
michael <71210704@students.ukdw.ac.id>
2021-11-11 20:36:13 +0700

    Soal 1

---
+++ 1_B_71210704.py
@@ -0,0 +1,7 @@
+#input
+Nama_lengkap = input("Nama : ")
+Tempat_tanggal_lahir = input("Tempat tanggal lahir : ")
+
+#cetak
+print("Haloo!",Nama_lengkap)
+print("Anda lahir di",Tempat_tanggal_lahir,"pada tanggal",Tempat_tanggal_lahir)

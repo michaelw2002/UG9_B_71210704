4d096ef94eb3ba3726e48292f7630487b7af407e
michael <71210704@students.ukdw.ac.id>
2021-11-11 20:37:49 +0700

    Soal 3

---
+++ 3_B_71210704.py
@@ -0,0 +1,10 @@
+Nilai1 = int(input("Nilai 1 : "))
+Nilai2 = int(input("Nilai 2 : "))
+Nilai3 = int(input("Nilai 3 : "))
+
+#proses
+Jumlah_nilai = Nilai1 + Nilai2 + Nilai3
+rata_rata = Jumlah_nilai / 3
+
+#print
+print("Rata-rata dari",Nilai1,"+",Nilai2,"+",Nilai3,"adalah",rata_rata)

c935bc8d53c6b8b805fc666b56922dc56f823c66
michael <71210704@students.ukdw.ac.id>
2021-11-11 20:37:00 +0700

    Soal 2

---
+++ 2_B_71210704.py
@@ -0,0 +1,9 @@
+#input
+r_alas_tabung = int(input("Masukkan jari-jari alas tabung : "))
+tinggi_tabung = int(input("Masukkan tinggi tabung : "))
+
+#proses
+volume_tabung = 3.14 * r_alas_tabung * r_alas_tabung * tinggi_tabung
+
+#cetak
+print("Volume tabung adalah : ", volume_tabung)
